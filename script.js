ed118b5f18b07408bc92655ad4375803c9ebc6b0
elizianeandreia <liziandreia@gmail.com>
2025-01-27 11:28:59 -0300

    criando constantes

---
+++ script.js
@@ -0,0 +1,12 @@
+const Contadorcalorias =document.getElementById('contador-calorias');
+const orcamentoNumeroInput = document.getElementById('');
+const listaEntrada = document.getElementById('');
+const addListaEntrada = document.getElementById('');
+const limparButton = document.getElementById('');
+const saida = document.getElementById('');
+
+let Erro =false;
+
+const limparInputString(str){
+    const regex = /\+-/;
+}
