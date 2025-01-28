a39418460f92699f35d788ba12a76a1a8fe78603
elizianeandreia <liziandreia@gmail.com>
2025-01-28 10:08:11 -0300

    finalizando funções e criando terceira

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const Contadorcalorias =document.getElementById('contador-calorias');
 const orcamentoNumeroInput = document.getElementById('orcamento');
 const listaEntrada = document.getElementById('lista-entrada');
-const addListaEntrada = document.getElementById('add-entrada');
+const addEntradaButton = document.getElementById('add-entrada');
 const limparButton = document.getElementById('limpar');
 const saida = document.getElementById('saida');
 
 let Erro =false;
 
 const limparInputString(str){
-    const regex = /\+-/;
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
 }
+
+ function isInvalidInput(str){
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+ }
+
+
+ function addEntrada(){
+    
+ }
