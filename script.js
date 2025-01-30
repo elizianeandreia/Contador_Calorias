575d9b414bdafc68a490045491c46d9fa9584877
elizianeandreia <liziandreia@gmail.com>
2025-01-30 09:37:02 -0300

    Montando estrutura da função para calcular calorias

--- script.js
+++ script.js
@@ -18,25 +18,42 @@ function isInvalidInput(str) {
 }
 
  function addEntrada() {
-    const targetInputContainer =document.querySelector(listaEntrada);
-    const entradaNumber = targetInputContainer.querySelectorAll();
+    const targetInputContainer =document.querySelector(`#${listaEntrada.value}.input-conatiner`);
+    const entradaNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString =`
-    <label></label>
-    <input>
-    <label></label>
-    <input>
-    `
-
+    <label for="${listaEntrada.value}-${entradaNumber}-nome">Entrada${entradaNumber} Nome</label>
+    <input type="text" id="${listaEntrada.value}-${entradaNumber}-nome" placeholder="Nome">
+    <label for="${listaEntrada.value}-${entradaNumber}-calorias">Entrada${entradaNumber} Calorias</label>
+    <input type="number" min="0" id="${listaEntrada.value}-${entradaNumber}-calorias" placeholder="Calorias"`>;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
-function calculadorCalorias (){
+function calculadorCalorias (e){
+e.preventDefault();
+Erro =false;
 
+const cafedamanhaNumberInputs = document.querySelectorAll();
+const almocoNumberInputs =document.querySelectorAll();
+const jantarNumberInputs =document.querySelectorAll();
+const aperitivosNumberInputs = document.querySelectorAll();
+const exerciciosNumberInputs =document.querySelectorAll();
+
+const cafedamanhaCalorias = getCaloriasFromInputs();
+const almocoCalorias =getCaloriasFromInputs();
+const jantarCalorias =getCaloriasFromInputs();
+const aperitivosCalorias =getCaloriasFromInputs();
+const exerciciosCalorias =getCaloriasFromInputs();
+const orcamentoCalorias =getCaloriasFromInputs();
+
+if(Erro){
+    return;
+}
 }
 
-function getCaloriasFromInput(){
+function getCaloriasFromInputs(){
 
 }
 
 function limparForm(){
-    
+
 }
