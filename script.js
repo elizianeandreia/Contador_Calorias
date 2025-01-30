e4b7ef87c01900fe32faef40c2c33d2b6e4314b9
elizianeandreia <liziandreia@gmail.com>
2025-01-30 09:44:56 -0300

    corrigindo pequenos erros.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const saida = document.getElementById('saida');
 
 let Erro =false;
 
-const limparInputString(str){
+function limparInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
 }
@@ -18,15 +18,18 @@ function isInvalidInput(str) {
 }
 
  function addEntrada() {
-    const targetInputContainer =document.querySelector(`#${listaEntrada.value}.input-conatiner`);
+    const targetInputContainer = document.querySelector(`#${listaEntrada.value} .input-container`);
     const entradaNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-    const HTMLString =`
-    <label for="${listaEntrada.value}-${entradaNumber}-nome">Entrada${entradaNumber} Nome</label>
-    <input type="text" id="${listaEntrada.value}-${entradaNumber}-nome" placeholder="Nome">
-    <label for="${listaEntrada.value}-${entradaNumber}-calorias">Entrada${entradaNumber} Calorias</label>
-    <input type="number" min="0" id="${listaEntrada.value}-${entradaNumber}-calorias" placeholder="Calorias"`>;
+    const HTMLString = `
+        <label for="${listaEntrada.value}-${entradaNumber}-nome">Entrada ${entradaNumber} Nome</label>
+        <input type="text" id="${listaEntrada.value}-${entradaNumber}-nome" placeholder="Nome">
+        <label for="${listaEntrada.value}-${entradaNumber}-calorias">Entrada ${entradaNumber} Calorias</label>
+        <input type="number" min="0" id="${listaEntrada.value}-${entradaNumber}-calorias" placeholder="Calorias">
+    `;
+
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
 
 function calculadorCalorias (e){
 e.preventDefault();
@@ -57,3 +60,5 @@ function getCaloriasFromInputs(){
 function limparForm(){
 
 }
+
+addEntradaButton.addEventListener("click", addEntrada);
